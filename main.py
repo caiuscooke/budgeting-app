16a717a3f65e0b696a7c23d77fb03bc6b8a96348
caiuscooke <cookecaius@gmail.com>
2024-01-25 18:16:39 -0500

    class 1/25

--- main.py
+++ main.py
@@ -1,28 +1,6 @@
-<<<<<<< HEAD
-from trans import trans
-view = 1
-add = 2
-edit = 3
-delete = 4
-print("welcome to the budgeting app.")
-option = int(input("type 1 for view 2 for add 3 for edit 4 for delete."))
-if option == view:
-    trans.view()
-elif option == add:
-    category = input("category of purchase ")
-    amount = float(input("amount of purchase "))
-    time = input("time of purchase ")
-    date = input("date of purchase ")
-
-    transaction = trans(category, amount, time, date)
-    transaction.save()
-
-# elif option== edit:#
-=======
 file = open("helloworld.txt", "w")
 file.write("10+10")
 file.close()
 
 with open("helloworld.txt", "w") as file:
     file.write("hello world")
->>>>>>> c572937650c21093efc24632e335bfa854ce3335
--- trans.py
+++ trans.py
@@ -1,19 +1,35 @@
-class trans:
-    def __init__(self, category: str, amount: float, time: str, date: str):
+class Transaction:
+    def __init__(self, category: str, amount: float, time: str, date: str, product: list):
         self.category = category
         self.amount = amount
         self.time = time
         self.date = date
-# create a txt:
+        self.product = product
 
+    # create a txt:
     def save(self):
         with open("trans.txt", "a") as file:
             file.write(
-                f"\n{self.category} {self.amount} {self.time} {self.date}")
-# creat a way to view the trans text file
+                f"\n{self.category} {self.amount} {self.time} {self.date} {self.product}")
 
-    def view():
-        with open("trans.txt", "r") as file:
-            lines = file.readlines()
-            for line in lines:
-                print(line)
+
+# create a way to view the trans text file
+def view():
+    with open("trans.txt", "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            print(line)
+
+
+def display_transactions():
+    transaction_date = input()
+    with open('trans.txt', 'r') as file:
+        transactions = file.readlines()
+        for each in transactions:
+            line_split = each.split()
+            date = line_split[3]
+            if transaction_date == date:
+                print(each)
+
+
+display_transactions()
