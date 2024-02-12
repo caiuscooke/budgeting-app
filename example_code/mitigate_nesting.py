ac115044c8e9f7e4770cd1d3b2e517f6481dfaf2
caiuscooke <cookecaius@gmail.com>
2024-02-11 20:32:13 -0500

    class 2/11

---
+++ example_code/mitigate_nesting.py
@@ -0,0 +1,23 @@
+def function1(param):
+    if param == 2:
+        # imagine 3 lines of code
+        print("undesirable condition")
+        return
+    # imagine many lines of code
+    # imagine many lines of code
+    # imagine many lines of code
+    # imagine many lines of code
+    # imagine many lines of code
+    # imagine many lines of code
+    # imagine many lines of code
+    # imagine many lines of code
+    # imagine many lines of code
+    # imagine many lines of code
+    # imagine many lines of code
+    # imagine many lines of code
+    # imagine many lines of code
+    # imagine many lines of code
+    print("desirable condition")
+
+
+function1(2)
