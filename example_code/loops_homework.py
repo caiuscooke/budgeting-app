04a842dae3ce3e3b91b598222bb02683c47a6ca2
caiuscooke <cookecaius@gmail.com>
2024-02-20 20:11:27 -0500

    2/20/24

---
+++ example_code/loops_homework.py
@@ -0,0 +1,6 @@
+# make a for loop that adds each letter of the a word to a list
+
+# make a for loop that checks if a letter in a word occurs 2 or more times
+
+# make a for loop that adds a random word to each entry in a list
+# ["hello", "world"] -> ["hello randomword1", "world randomword2"]
--- login.py
+++ login.py
@@ -1,4 +1,4 @@
-from string import ascii_uppercase
+from string import ascii_uppercase, ascii_lowercase
 
 class User:
     def __init__(self, username, password):
@@ -7,6 +7,8 @@ class User:
 
     def save(self):
         with open("login.txt", "a") as file:
+            # apply password hashing here
+            # hashed = self.password
             file.write(
                 f"\n{self.username} {self.password}")
 
@@ -16,28 +18,67 @@ def create_user():
     # now we're in this function
     
     # ask for username
+    print("Enter your username")
+    username = input() # asdf;lkajsdf
+    if " " in username:
+        return create_user()
+    
     # open the text file
-    # read the textfile into a list (aka use readlines())
+    with open("login.txt", "r") as file:
+        # read the textfile into a list (aka use readlines())
+        lines = file.readlines() # ["caiuscooke al;skdjf908234jlasdf"]
         # for each item in that list
+        for each in lines:
             # if username in each
+            if username in each:
                 # notify user that name is already taken
+                print("That username is already taken")
                 # recursively call the create_user function
-    # list of unaccepted characters
+                return create_user()
+            
+    # list of accepted characters
+    accepted_char_list = ["!", "#", "%", "&", "?", "@", "￥", "$"] 
+    acceptable_num_list = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     # print a statement of the requirements (one capital, one number, one special character)
-    # print unacceptable characters
+    print(
+        "Password must have at least one lowercase, "
+        + "one uppercase, one special character, and one number"
+    )
+    # print acceptable characters
+    accepted_char_str = " ".join(accepted_char_list)
+    print(f"The accepted characters are {accepted_char_str}")
     # while True
+    while True:
         # ask for password
+        first_password = input("Enter your password: ")
         # ask for password again
+        confirmation_password = input("Enter your password again: ")
         # for each in password
-            # if each in unaccepted characters
-                # print that the user has chosen an unacceptable character
-                # continue
+        has_capital, has_symbol, has_number = False
+
+        for each in first_password: # 1L7xvb#09!
+            
             # if each is in a list of capital letters
+            if each in ascii_uppercase:
                 # set a variable called has_capital to true
-            # if each is in a list of acceptable symbols
+                has_capital = True
+            # elif each is in a list of acceptable symbols
+            elif each in accepted_char_list:
                 # set a variable called has_symbol to true
-            # if each is 1 - 0
+                has_symbol = True
+            # elif each is 1 - 0
+            elif each in acceptable_num_list:
                 # set a variable called has_number to true
+                has_number = True
+            elif each in ascii_lowercase:
+                continue
+            # else
+            else:
+                # tell the user that the character they inputed was unacceptable
+                print("You have an unaccepted character in your chosen password")
+                # continue
+                continue
+            
         # if confirmation matches and (has_capital, has_symbol, and has_number is true)
             # break 
     # confirm username and password to user
