ee810b60f352a45006b4883bdfef6ada5232b6c4
dlodom92 <148286008+dlodom92@users.noreply.github.com>
2024-02-21 21:19:05 -0500

    hw

--- example_code/loops_homework.py
+++ example_code/loops_homework.py
@@ -1,6 +1,16 @@
 # make a for loop that adds each letter of the a word to a list
+# ui = input("type a word?: ")
+# al = []
+# for each in ui:
+#     al = [each]
+# print(al)
 
 # make a for loop that checks if a letter in a word occurs 2 or more times
-
+# test word
+ui = "cheese"
+# for each letter in test word
+for each in ui:
+    if ui.count(each) > 1:
+        print("that letter repeats?")
 # make a for loop that adds a random word to each entry in a list
 # ["hello", "world"] -> ["hello randomword1", "world randomword2"]
