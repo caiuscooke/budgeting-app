d234b3f1f63c373561ee5abb95d0f514ab5e7e50
caiuscooke <cookecaius@gmail.com>
2024-04-02 15:43:25 -0400

    04/02

--- edit.py
+++ edit.py
@@ -1,12 +1,32 @@
-def get_edit_info():
-    from datetime import datetime
-    from get_input import convert_input_to_datetime
-    from file_manipulation import get_txt_lines
-    print("do you want to edit one  or multiple transactions")
-    print("What do you want to edit in transactions?: date, category, amount, time")
-    user_input_as_date = input()
-    user_input_as_date = convert_input_to_datetime()
-    transactions = get_txt_lines(user_input_as_date)
-    edit_item_list = transactions
-    for  in edit_item_list:
+# def get_edit_info():
+#     from datetime import datetime
+#     from get_input import convert_input_to_datetime
+#     from file_manipulation import get_txt_lines
+#     print("do you want to edit one  or multiple transactions")
+#     print("What do you want to edit in transactions?: date, category, amount, time")
+#     user_input_as_date = input()
+#     user_input_as_date = convert_input_to_datetime()
+#     transactions = get_txt_lines(user_input_as_date)
+#     edit_item_list = transactions
+#     for  in edit_item_list:
 
+# find a transaction to edit ==> selected by user input
+# once transaction is found, 
+# which element (i.e. date, time, cost, etc.) do you want to edit? ==> selected by user input
+# possibly isolate the transaction so that there's no side effects ==> overwriting the file 
+# only changes what the user changed
+# then change the item the user wants to edit ==> overwrite the file
+from get_input import convert_input_to_datetime
+from file_manipulation import get_txt_lines
+
+def edit_transaction():
+    user_input_as_date = convert_input_to_datetime()
+    transactions = get_txt_lines(user_input_as_date)    
+    # we can't use get_txt_lines because
+    # we need to use the whole txt file, not just a list of the 
+    # items 
+    
+    # for loop
+    # split and see if date matches user_input_as_date
+    # nested loop for each item in that split
+    # make the edits in the nested for loop
--- example_code/recursive_returns.py
+++ example_code/recursive_returns.py
@@ -29,12 +29,29 @@ def recursive_function(number: int) -> bool:
 
 # example:
 # x = 10
-# return should be the value of 1 * 2 * 3 * ... * 10
-# def factorial(x):
-#     if x <= 1:
-#         return 1
-#     else:
-#         return (x * factorial(x - 1))
-
+# return should be the value of -5 * 1 * 2 * 3 * ... * 10
+def factorial(x):
+    if x <= 1:
+        return 1
+    else:
+        return (x * factorial(x - 1))
 # print(factorial(5))
 
+# sum([5, 8, 10, 12])
+    # 5 + sum([8, 10, 12])
+        # 8 + sum([10, 12])
+            # 10 + sum([12])
+                # return 12
+# current item (some math operation +/*-) recursive(current item (-+ 1))
+
+# empty += current_item 
+# return current_item + recursive_call(next_item) 
+def new_sum(num_list):
+    if len(num_list) == 1:
+        return num_list[0]
+    else:
+        return num_list[0] + new_sum(num_list[1:])
+    
+num_list = [5, 8, 10, 12]
+sum = new_sum(num_list)
+print(sum)
