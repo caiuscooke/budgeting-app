d41587ea0a7a1428e39a182e662e795d0671b0eb
dlodom92 <148286008+dlodom92@users.noreply.github.com>
2024-05-20 18:37:00 -0400

    cha

--- reports.py
+++ reports.py
@@ -10,7 +10,8 @@ last_month_int = today_dt.month - 1
 first_day_current_month = today_dt.replace(day=1)
 last_day_last_month = first_day_current_month - timedelta(days=1)
 first_day_last_month = first_day_current_month.replace(month=last_month_int)
-
+last_month_transactions = []
+total = 0
 # for each line in the transactions list ===>>> for transaction in transactions_list
 for transaction in transactions_list:
     transaction_date = transaction.split()[3]
@@ -19,15 +20,19 @@ for transaction in transactions_list:
 # 	convert transaction_date to a datetime object
     transaction_datetime = datetime.strptime(transaction_date, "%Y-%m-%d")
 # 	if the datetime object is between the first day of last month and the last day of last month
-    if (first_day_last_month <= transaction_datetime 
-        and transaction_datetime <= last_day_last_month):
-    # 		add LINE to an empty list called "last_month_transactions"
+    if (first_day_last_month <= transaction_datetime
+            and transaction_datetime <= last_day_last_month):
+        # 		add LINE to an empty list called "last_month_transactions"
+        last_month_transactions = last_month_transactions.append(
+            transaction_datetime)
 
     # for each transaction in last_month_transactions
+for transaction in last_month_transactions:
     # 	split the transaction into a second "transaction_split"
+    amount = transaction.split()[1]
     # 	get the amount from transaction_split and store it in "amount" after casting to float
     # 	add the amount variable to an empty number variable called "total"
-
+    amount += total
     # print "You spent {total} from {first day of last month (i.e. 04.01.24)} to {last day of last month (i.e. 04.30.24)}"
     # open txt file for monthly totals
     # 	write total and month name to txt file
